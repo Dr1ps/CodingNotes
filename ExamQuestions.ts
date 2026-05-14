e331707775af83ad6e7a1ca74fa6afd0f479ffaa
Gabriele Perna <gabrigionny@gmail.com>
2026-05-15 01:22:02 +0200

    added exam questions

---
+++ ExamQuestions.ts
@@ -0,0 +1,85 @@
+//QUESTIONS FROM ORAL EXAM - LAB.1
+
+/*
+The teacher gives you 2 exercises to do in 25 minutes (god damn)
+
+1) Difference between var, let, const
+
+2) Difference between _proto_ and .prototype
+
+3) How do accessibility modifiers work and which are they?
+
+C'è public, private, protected.
+Public = accessibili al di fuori della classe
+Private = accessibili solo all'interno della classe
+Protected = accessibile all'interno della classe e delle sottoclassi
+Specificare anche la cosa del transpiler, come si traduce in JS
+
+4) What are getters and setters for
+
+5) Cosa vuol dire sottoclasse o quando una classe estende un altra classe?
+
+6) Differenza tra semantica per valore e per riferimento
+
+7) Cosa è Hoisting / oisting?
+
+8) Cosa sono le interfacce e a cosa servono
+
+9) Cosa sono i generics e a cosa servono
+
+10) Differenza tra tipizzazione in JS e TS
+
+TS richiede che le variabili vengano tipizzate
+In JS c'è inferenza
+
+11) Se io assegno a x una stringa in JS, dopo posso assegnargli un numero?
+
+In JS si, TS no
+
+12) Come si chiama il fatto che in TS la variabile non può cambiare valore?
+
+Tipizzazione statica
+
+13) Differenza tra any e unknown
+Attenzione: riposta non definitiva ma quasi giusta penso
+Any = annulla i controlli sul tipo, permette un qualsiasi tipo
+Unknown = il tipo non si sa ancora e quindi sono sempre necessari controlli
+
+14) cosa è un modulo (per le librerie) (esistono solo in TS se non sbaglio?)
+
+15) che "word" devo mettere per esportare una classe?
+Export
+
+16) Come si fa a fare in modo che una variabile assuma tot valori specifici
+
+tipo unione
+"a" | true | 1
+
+17) differenza tra interfaccia extends interfaccia e class extends class (ereditarieta tra interfacce e classi)
+
+18) Ho un oggetto di una classe Figlio che estende una interfaccia che a sua volta estende un interfaccia, cosa si trova nel prototipo?
+RIsposta data, non sono sicuro sia giusta:
+Le cose dell'interfaccia non ci sono, della classe si.
+
+19) 
+
+
+COSE STRANE DA GUARDARE ASSOLUTAMENTE
+
+Stesso generics usato 2 volte da qualche parte
+
+Tipo unione??? in ambito di interfacce
+
+Cosa è MAP
+
+Regex
+
+IMPORTANTE: metodi e attributi ereditati di una classe sono salvati nel prototype!!!!!
+
+
+
+
+
+
+
+*/
